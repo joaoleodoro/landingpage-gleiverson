769084217690a3ab499f46bd224f72935e68e823
João Leodoro <joaooleodoro@gmail.com>
2025-02-04 17:24:09 -0300

    add nova section e imgs

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,26 @@ $(document).ready(function() {
         $('.btn-mobile').find('i').toggleClass('fa-x');
     });
 });
+
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const track = document.querySelector('.carousel-track');
+const cards = document.querySelectorAll('.card');
+const cardWidth = cards[0].offsetWidth + 15;
+
+prevBtn.addEventListener('click', () => {
+    if (track.scrollLeft === 0) {
+        track.scrollLeft = track.scrollWidth;
+    } else {
+        track.scrollBy({ left: -cardWidth, behavior: 'smooth' });
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (track.scrollLeft + track.clientWidth >= track.scrollWidth - 1) {
+        track.scrollLeft = 0;
+    } else {
+        track.scrollBy({ left: cardWidth, behavior: 'smooth' });
+    }
+});
+
